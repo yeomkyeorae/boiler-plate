b93fa4eaa88ad8d1207bc837cf7b71155f4e002a
yeomkyeorae <duarufp06@naver.com>
2020-08-15 15:10:17 +0900

    bcrypt

--- models/User.js
+++ models/User.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const saltRounds = 10; // salt를 만들 때의 자릿수
 
 const userSchema = mongoose.Schema({
   name: {
@@ -30,6 +32,25 @@ const userSchema = mongoose.Schema({
     type: Number
   }
 });
+
+userSchema.pre("save", function(next) {
+  // 비밀번호를 암호화 시킨다
+  let user = this;
+
+  if (user.isModified("password")) {
+    bcrypt.genSalt(saltRounds, function(err, salt) {
+      if (err) return next(err);
+
+      bcrypt.hash(user.password, salt, function(err, hash) {
+        if (err) return next(err);
+        user.password = hash;
+        next();
+      });
+    });
+  } else {
+    next();
+  }
+}); // user 정보를 저장하기 전에 콜백을 실행하게
 
 const User = mongoose.model("User", userSchema);
 
