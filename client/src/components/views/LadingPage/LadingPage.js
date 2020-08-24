205e4c910635ba7f7f1d5c877e94bcf2473a2acb
yeomkyeorae <duarufp06@naver.com>
2020-08-24 22:08:02 +0900

    logout

--- client/src/components/views/LadingPage/LadingPage.js
+++ client/src/components/views/LadingPage/LadingPage.js
@@ -1,12 +1,22 @@
 import React, { useEffect } from "react";
 import axios from "axios";
 
-function LadingPage() {
+function LadingPage(props) {
   useEffect(() => {
     axios
       .get("http://localhost:5000/api/hello")
       .then(response => console.log(response.data));
   }, []);
+
+  const onClickHandler = () => {
+    axios.get(`/api/users/logout`).then(response => {
+      if (response.data.success) {
+        props.history.push("/login");
+      } else {
+        alert("로그아웃이 실패했습니다");
+      }
+    });
+  };
 
   return (
     <div
@@ -19,6 +29,7 @@ function LadingPage() {
       }}
     >
       <h2>시작 페이지</h2>
+      <button onClick={onClickHandler}>로그아웃</button>
     </div>
   );
 }
